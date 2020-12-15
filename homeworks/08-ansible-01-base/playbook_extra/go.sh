3ed09c88d3ffea2514470792cb97351af176521d
Oleg Ananyev <oleg@ananyev.me>
2020-12-15 22:25:33 +0300

    final

--- homeworks/08-ansible-01-base/playbook_extra/go.sh
+++ homeworks/08-ansible-01-base/playbook_extra/go.sh
@@ -2,5 +2,5 @@
 docker run --name ubuntu -d pycontribs/ubuntu:latest sleep 60000
 docker run --name centos7 -d pycontribs/centos:7 sleep 60000
 docker run --name fedora -d pycontribs/fedora:latest sleep 60000
-ansible-playbook -i inventory/prod.yml site.yml
+ansible-playbook -i inventory/prod.yml --ask-vault-pass site.yml
 docker rm -f ubuntu centos7 fedora
