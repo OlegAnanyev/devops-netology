1e26db507851b3ebac4629799edfc22064cd3cc7
Oleg Ananyev <oleg@ananyev.me>
2020-12-15 22:14:48 +0300

    extra

---
+++ homeworks/08-ansible-01-base/playbook_extra/go.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker run --name ubuntu -d pycontribs/ubuntu:latest sleep 60000
+docker run --name centos7 -d pycontribs/centos:7 sleep 60000
+docker run --name fedora -d pycontribs/fedora:latest sleep 60000
+ansible-playbook -i inventory/prod.yml site.yml
+docker rm -f ubuntu centos7 fedora
